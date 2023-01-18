879b012db9e1342112aef3ea19dda998565ab456
Windson Macedo <windsondev@hotmail.com>
2023-01-18 18:11:40 -0300

    feat: classe Archetype criada

---
+++ src/Archetypes/Archetype.ts
@@ -0,0 +1,31 @@
+import { EnergyType } from '../Energy';
+
+export default abstract class Archetype {
+  readonly _name: string;
+  readonly _special: number;
+  readonly _cost: number;
+
+  constructor(name: string) {
+    this._name = name;
+    this._special = 0;
+    this._cost = 0;
+  }
+
+  public get name() {
+    return this._name;
+  }
+
+  public get special() {
+    return this._special;
+  }
+
+  public get cost() {
+    return this._cost;
+  }
+
+  public static createdArchetypeInstances(): number {
+    throw new Error('Not implemented');
+  }
+
+  public abstract get energyType():EnergyType;
+}
