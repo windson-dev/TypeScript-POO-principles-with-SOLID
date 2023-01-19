fa062c13ed38f6d2121df434120fdb672a769d9f
Windson Macedo <windsondev@hotmail.com>
2023-01-18 21:08:23 -0300

    fix: corrigido import no Archetypes/index.ts

--- src/Archetypes/index.ts
+++ src/Archetypes/index.ts
@@ -1,5 +1,5 @@
 import Archetype from './Archetype';
-import Mage from './mage';
+import Mage from './Mage';
 import Necromancer from './Necromancer';
 import Ranger from './Ranger';
 import Warrior from './Warrior';
